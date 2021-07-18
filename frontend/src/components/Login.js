5b392fae96b0a95bf556ec4bcf2ec83e98895c2b
Kevin B. Coleman <kevinb.coleman@Kevins-MBP.fios-router.home>
2021-07-18 02:00:19 -0400

    register/login style with responsive and mobile version

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { Form, Row, Col, Button, Container } from 'react-bootstrap'
+import { Form, Row, Col, Button, Container, Card } from 'react-bootstrap'
 import { LinkContainer, Redirect } from 'react-router-bootstrap'
 import AuthContext from '../context/auth/AuthContext'
 import AlertContext from '../context/alert/alertContext'
@@ -42,36 +42,76 @@ const Login = (props) => {
   }
 
   return (
-    <Container className="Form">
-      <h1>Login</h1>
-      <Form className="" onSubmit={onSubmit}>
-        <Form.Group>
-          <Form.Label htmlFor="email"></Form.Label>
-          <Form.Control
-            name="email"
-            type="text"
-            onChange={onChange}
-            value={email}
-            placeholder="EMAIL"
-            required
-          >
-          </Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="password"></Form.Label>
-          <Form.Control
-            name="password"
-            type="password"
-            onChange={onChange}
-            value={password}
-            placeholder="PASSWORD"
-            required
-          >
-          </Form.Control>
-        </Form.Group>
-        <Button type="submit">Sign In</Button>
+    <>
+
+      <Form onSubmit={onSubmit} className="Form FormDesktop">
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=653&q=80"
+            className="loginDisplay"
+          />
+        </div>
+        <Card className="FormCard">
+          <Form.Group>
+            <Form.Label htmlFor="email"></Form.Label>
+            <Form.Control
+              name="email"
+              type="text"
+              onChange={onChange}
+              value={email}
+              placeholder="EMAIL"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="password"></Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              onChange={onChange}
+              value={password}
+              placeholder="PASSWORD"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Button type="submit">Log In</Button>
+        </Card>
       </Form>
-    </Container>
+
+
+      <div className="Form FormMobile">
+        <h1>Login</h1>
+        <Form className="" onSubmit={onSubmit}>
+          <Form.Group>
+            <Form.Label htmlFor="email"></Form.Label>
+            <Form.Control
+              name="email"
+              type="text"
+              onChange={onChange}
+              value={email}
+              placeholder="EMAIL"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="password"></Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              onChange={onChange}
+              value={password}
+              placeholder="PASSWORD"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Button type="submit">Sign In</Button>
+        </Form>
+      </div>
+    </>
   )
 }
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { Form, Row, Col, Button, Container } from 'react-bootstrap'
+import { Form, Row, Col, Button, Container, Card } from 'react-bootstrap'
 import { LinkContainer, Redirect } from 'react-router-bootstrap'
 import AuthContext from '../context/auth/AuthContext'
 import AlertContext from '../context/alert/alertContext'
@@ -44,50 +44,107 @@ const Register = (props) => {
     })
   }
   return (
-    <Container className="Form">
-      <h1>Register</h1>
-      <Form onSubmit={onSubmit}>
-        <Form.Group>
-          <Form.Label htmlFor="username"></Form.Label>
-          <Form.Control
-            name="username"
-            type="text"
-            onChange={onChange}
-            value={username}
-            placeholder="USERNAME"
-            required
-          >
-          </Form.Control>
-        </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor="email"></Form.Label>
-          <Form.Control
-            name="email"
-            type="text"
-            onChange={onChange}
-            value={email}
-            placeholder="EMAIL"
-            required
-          >
-          </Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="password"></Form.Label>
-          <Form.Control
-            name="password"
-            type="password"
-            onChange={onChange}
-            value={password}
-            placeholder="PASSWORD"
-            minLength="6"
-            required
-          >
-          </Form.Control>
-        </Form.Group>
-        <Button type="submit">Sign Up</Button>
+    <>
+
+      <Form onSubmit={onSubmit} className="Form FormDesktop FormRegister">
+        <h1 className="registerHeader order-3">Discover food that works for you.</h1>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=653&q=80"
+            className="loginDisplay"
+          />
+        </div>
+        <Card className="FormCard RegisterCard">
+          <Form.Group>
+            <Form.Label htmlFor="username"></Form.Label>
+            <Form.Control
+              name="username"
+              type="text"
+              onChange={onChange}
+              value={username}
+              placeholder="USERNAME"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label htmlFor="email"></Form.Label>
+            <Form.Control
+              name="email"
+              type="text"
+              onChange={onChange}
+              value={email}
+              placeholder="EMAIL"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="password"></Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              onChange={onChange}
+              value={password}
+              placeholder="PASSWORD"
+              minLength="6"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Button type="submit">Sign Up</Button>
+        </Card>
       </Form>
-    </Container>
+
+
+      <div className="Form FormMobile">
+        <h1>Register</h1>
+        <Form onSubmit={onSubmit}>
+          <Form.Group>
+            <Form.Label htmlFor="username"></Form.Label>
+            <Form.Control
+              name="username"
+              type="text"
+              onChange={onChange}
+              value={username}
+              placeholder="USERNAME"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label htmlFor="email"></Form.Label>
+            <Form.Control
+              name="email"
+              type="text"
+              onChange={onChange}
+              value={email}
+              placeholder="EMAIL"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="password"></Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              onChange={onChange}
+              value={password}
+              placeholder="PASSWORD"
+              minLength="6"
+              required
+            >
+            </Form.Control>
+          </Form.Group>
+          <Button type="submit">Sign Up</Button>
+        </Form>
+      </div>
+
+    </>
   )
 }
 
