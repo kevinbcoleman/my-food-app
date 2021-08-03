604b2ec7767368f9d13f0a575fb5b526e97e62d2
Kevin B. Coleman <kevinb.coleman@Kevins-MBP.fios-router.home>
2021-08-03 01:22:02 -0400

    made header links responsive to route changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { useRouteMatch, useLocation, useHistory } from 'react-router-dom'
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import AuthContext from '../context/auth/AuthContext'
@@ -6,32 +7,56 @@ import recipeContext from '../context/recipe/recipeContext'
 import '../App.css';
 
 
-const Header = ({ props }) => {
+const Header = () => {
   const authContext = useContext(AuthContext)
   const { isAuthenticated, logout, user } = authContext
   const { clearRecipes } = recipeContext
-  const [profVis, setProfVis] = useState("visible")
+  const [profShow, setProfShow] = useState('')
+  const match = useRouteMatch("/")
+  const location = useLocation()
+  const history = useHistory()
+
+  const getCurrentRoute = () => {
+    // if (location.pathname === "/profile" || "/profile/myrecipes" ||
+    //   "/profile/savedrecipes") {
+    //   setProfShow('')
+    // } else {
+    //   setProfShow('Profile')
+    // }
+  }
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setProfShow('Profile')
+    } else if (location.pathname.includes('/prof')) {
+      setProfShow('')
+    }
+    //  === "/profile" || "/profile/myrecipes" || "/profile/savedrecipes") {
+  }
+    // getCurrentRoute()
+    , [location])
+
 
   const onLogout = () => {
     logout()
     clearRecipes()
-    props.history.push('/')
+    history.push('/')
   }
 
-  const handleShow = () => {
-    setProfVis("visible")
-  }
+  // const handleShow = () => {
+  //   setProfVis("visible")
+  // }
 
-  const handleHide = () => {
-    setProfVis("hidden")
-  }
+  // const handleHide = () => {
+  //   setProfVis("hidden")
+  // }
 
   return (
     <>
 
       <Navbar className="Navbar" bg="light" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer className="textStyle" onClick={handleShow} to="/">
+          <LinkContainer className="textStyle" to="/">
             <Navbar.Brand>Food for Life</Navbar.Brand>
           </LinkContainer>
 
@@ -48,8 +73,8 @@ const Header = ({ props }) => {
                 </LinkContainer>
               </> :
               <>
-                <LinkContainer className="textStyle" style={{ "visibility": profVis }} onClick={handleHide} to="/profile">
-                  <Nav.Link className="ml-auto p-link">My Profile</Nav.Link>
+                <LinkContainer className="textStyle" to="/profile">
+                  <Nav.Link className="ml-auto p-link">{profShow}</Nav.Link>
                 </LinkContainer>
                 <LinkContainer className="textStyle" to='/'>
                   <Nav.Link onClick={onLogout}>Logout</Nav.Link>
@@ -63,5 +88,6 @@ const Header = ({ props }) => {
     </>
   )
 }
+
 
 export default Header
