73a76ce547f6e5e0aa041ad0bfc86216b19f9d88
Kevin B. Coleman <kevinb.coleman@Kevins-MBP.fios-router.home>
2021-08-08 14:01:21 -0400

    typo

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/auth', authController)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'))
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 }
 
 // LISTEN
